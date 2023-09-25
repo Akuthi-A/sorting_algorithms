2de0337c731c3d9f13c2134de6a020b0b0558086
Akuthi <akuthiapril99@gmail.com>
2023-09-25 13:24:05 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - implementation of the bubble sort algo
+ * @array: array to sort
+ * @size: size of an array
+ *
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swap_flag;
+
+	i = 0;
+	j = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swap_flag = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap_flag = 1;
+
+				print_array(array, size);
+			}
+		}
+		if (swap_flag == 0)
+		{
+			break;
+		}
+	}
+}
